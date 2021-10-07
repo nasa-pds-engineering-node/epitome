5906cba66a99aceb243ad020befedef77ec1279f
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2021-10-07 10:45:50 -0700

    Revert "Breaking the tests for verification of NASA-PDS/roundup-action#70"
    This reverts commit a4ea5cf8770a976bb9f5c124d6ecb12f8642e68c.

--- tests/pds/your_package_name/test_example.py
+++ tests/pds/your_package_name/test_example.py
@@ -5,9 +5,6 @@ class HelloWorldTests(unittest.TestCase):
     def test_example(self):
         assert True
 
-    def test_a_dumb_test(self):
-        assert False
-
 
 if __name__ == "__main__":
     unittest.main()
