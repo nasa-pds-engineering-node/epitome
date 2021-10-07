a4ea5cf8770a976bb9f5c124d6ecb12f8642e68c
Michael Joyce <Michael.J.Joyce@jpl.nasa.gov>
2021-10-07 10:31:29 -0700

    Breaking the tests for verification of NASA-PDS/roundup-action#70

--- tests/pds/your_package_name/test_example.py
+++ tests/pds/your_package_name/test_example.py
@@ -5,6 +5,9 @@ class HelloWorldTests(unittest.TestCase):
     def test_example(self):
         assert True
 
+    def test_a_dumb_test(self):
+        assert False
+
 
 if __name__ == "__main__":
     unittest.main()
